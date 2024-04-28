bfbf4dc748f1b0f28768303844e2cafd00ea7643
Fedor Rushkevich <fedrushkevich@gmail.com>
2024-04-28 15:12:08 +0300

    (FH-f): fix better housing.service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location'; 
 
 
 @Component({
@@ -9,16 +10,18 @@ import { ActivatedRoute } from '@angular/router';
   imports: [],
   template: `
     <p>
-      details works! {{housingLocationId}}
+      details works! {{housingLocation?.id}}
     </p>
   `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
  route: ActivatedRoute =  inject(ActivatedRoute);
- housingLocationId = 0
+ housingService = inject(HousingService);
+ housingLocation: HousingLocation | undefined;
 
  constructor() {
-  this.housingLocationId = Number(this.route.snapshot.params['id']);
+   const housingLocationId = Number(this.route.snapshot.params['id']);
+   this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
  }
 }
