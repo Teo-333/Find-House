0ac035f9fab7b99c2340796cdd7020dc2120d524
Fedor Rushkevich <fedrushkevich@gmail.com>
2024-04-28 15:44:54 +0300

    (FH-f): feature apply form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location'; 
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { first, last } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
     <article class="details">
       <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior photo of {{housingLocation?.name}}">
@@ -24,7 +25,17 @@ import { HousingLocation } from '../housing-location';
         </section>
         <section class="listing-apply">
           <h2 class="section-heading">Apply now to live</h2>
-          <button class="primary" type="button">Apply Now</button>
+          <form [formGroup]="applyForm">
+            <label for="firs-name">First Name</label>
+            <input type="text" id="first-name" formControlName="firstName">
+
+            <label for="last-name">Last Name</label>
+            <input type="text" id="last-name" formControlName="lastName">
+
+            <label for="email">Email</label>
+            <input type="text" id="email" formControlName="email">
+            <button class="primary" type="submit">Apply Now</button>
+         </form>
         </section>
       </section>
     </article>
@@ -35,6 +46,13 @@ export class DetailsComponent {
  route: ActivatedRoute =  inject(ActivatedRoute);
  housingService = inject(HousingService);
  housingLocation: HousingLocation | undefined;
+ applyForm = new FormGroup(
+  {
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  }
+);
 
  constructor() {
    const housingLocationId = Number(this.route.snapshot.params['id']);
